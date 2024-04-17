e5be6012f3d65dfe9125710f162a42c65c7ce60d
yoricky42 <yorickyves42@gmail.com>
2024-04-17 10:19:38 +0000

    Toutes la configuration est à jour et prête

--- assets/js/angular/BACKOFFICE.js
+++ assets/js/angular/BACKOFFICE.js
@@ -1,5 +1,9 @@
-var app = angular.module('c2ci', ['angular.filter','socialLogin', 'ngCookies', 'ngSanitize', 'ui.bootstrap', 'pascalprecht.translate']);
+var app = angular.module('started_project', ['angular.filter','socialLogin', 'ngCookies', 'ngSanitize', 'ui.bootstrap', 'pascalprecht.translate']);
 
 //---BASE_URL----//
-var BASE_URL = '//' + location.host + '/c2ci_back/public/';
+var BASE_URL = '//' + location.host + '/started_project_back/';
 
+app.controller('ProjectBackController', ($scope) =>{
+    console.log('Le back office est branché !');
+    document.querySelector('#url_de_base').innerHTML=location.host + '/started_project_back/';
+});
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,scss}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
